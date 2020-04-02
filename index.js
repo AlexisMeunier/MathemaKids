5e1c7b5c750be0744d8ec4e0b1e03f04656c044c
AlexisMeunier <meunier_33@live.fr>
2020-04-02 20:03:53 +0200

    message en cas de mauvaise réponse et modification du titre

--- index.js
+++ index.js
@@ -105,6 +105,10 @@ class Game
          this.speaker.speak('Bravo, '+ this.solution);
          this.win = true;
       }
+      else if( this.response.length === this.solution.length)
+      {
+         this.speaker.speak('Ce n\'est pas la bonne réponse tu peux cliquer sur effacer et recommencer');
+      }
    }
 
    refresh_input()
